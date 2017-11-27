877f26fa5c8186bc5dee4fd6e2d4d78157443999
uzi harband <uharband@cisco.com>
2017-11-27 17:28:45 +0200

    removed unused scripts and images

--- server.js
+++ server.js
@@ -20,28 +20,16 @@ app.get('/ping', function (req, res) {
 
 app.get('/manage/alive', function (req, res) {
     var live = {};
-    live.message = "hello-node is alive!";
+    live.message = "narrator is alive!";
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(live));
 });
 
 app.get('/manage/health', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
-    if(statusObj.crucialDependency1.status.toUpperCase() !== 'UP'){
-        res.statusCode = 500;
-    }
     res.send(JSON.stringify(statusObj));
 });
 
-app.get('/manage/status/:status', function (req, res) {
-    if(setStatus(req.params.status)){
-        res.send('OK. Set status to ' + req.params.status);
-    }
-    else
-    {
-        res.send("NOT OK. Didn't recognize the status you sent. please send either UP or DOWN. and no. i'm not case sensitive");
-    }
-});
 
 // upload an audio file
 app.post('/upload', function(req, res) {
@@ -62,11 +50,8 @@ app.get('/article', function(req, res){
     request(url, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        //console.log('body:', body); // Print the HTML for the Google homepage.
-        //res = response
         body = body.replace("<div class='art_header_footer_facebook'>", contents + "<div class='art_header_footer_facebook'>");
         res.send(body);
-        //res.end('ok');
     });
 
 });
@@ -90,8 +75,5 @@ function initiateStatus(){
     statusObj.initialReadiness = {};
     statusObj.initialReadiness.status = "UP";
     statusObj.initialReadiness.lastStatusUpdate = new Date().toISOString();
-
-    statusObj.crucialDependency1 = {};
-    setStatus('UP');
 }
 
